f16b584462cea06a399d5d1b520c7b1dd34b8aa1
haojunchuan <1562868065@qq.com>
2020-08-02 20:36:06 +0800

    刚创建项目

---
+++ src/main/java/com/hao/MathstudyApplication.java
@@ -0,0 +1,13 @@
+package com.hao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathstudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathstudyApplication.class, args);
+    }
+
+}
